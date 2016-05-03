bcce1acaf408f37bc840eb2aee81ae0eb434b7b6
Timofey Kondrashov <tkondrashov@gmail.com>
2016-05-03 23:40:52 +0100

    pitchers now have proper opponent team information

--- player_stats/player_stats/spiders/lineups.py
+++ player_stats/player_stats/spiders/lineups.py
@@ -11,20 +11,25 @@ class LineupsSpider(scrapy.Spider):
 	def parse(self, response):
 		for game in response.css('.game'):
 			teams = []
-			players = 0
 			for team_header in game.css('.team-data'):
 				teams.append(team_header.css('.team-name').xpath('text()').extract()[0])
+
+			pitcher_number = 0
+			for team_header in game.css('.team-data'):
 				player = PlayerStats()
 				player['name'] = team_header.css('.player-link').xpath('text()').extract()[0]
 				player['position'] = 'P'
 				player['hand'] = team_header.xpath('div/div/text()').extract()[1][2]
-				player['team'] = teams[-1]
+				player['team'] = teams[pitcher_number]
+				player['against'] = teams[-1 + pitcher_number]
 				bref = team_header.css('.player-link').xpath('@data-bref').extract()[0]
 				url = "http://www.baseball-reference.com/players/split.cgi?id=" + bref + "&year=2016&t=p"
 				request = scrapy.Request(response.urljoin(url), self.parse_pitcher_stats)
 			 	request.meta['item'] = player
+			 	pitcher_number += 1
 				yield request
 
+			players = 0
 			for player_div in game.css('.players > div'):
 				players += 1
 				player = PlayerStats()
@@ -60,7 +65,8 @@ class LineupsSpider(scrapy.Spider):
 				else:
 					if row_type == '':
 						break
-					player[row_type][self.hcolumns[cell_num-1]] = float(cell.xpath('text()').extract()[0])
+					if cell.xpath('text()').extract():
+						player[row_type][self.hcolumns[cell_num-1]] = float(cell.xpath('text()').extract()[0])
 				cell_num += 1
 
 		yield player
